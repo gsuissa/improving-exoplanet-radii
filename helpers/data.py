e7966d2d3230cfbfa9eb6d5e3309776fb261358c
Gabrielle Suissa <ge2205@columbia.edu>
2024-04-23 20:44:11 -0700

    adding helper functions and files

---
+++ helpers/data.py
@@ -0,0 +1,111 @@
+import lightkurve as lk
+import astropy.units as u
+import numpy as np
+
+from astropy.time import Time
+
+
+def get_flattened_lc(name, mission=None, exptime=None, method='pld'):
+    """Returns stitched and flattened light curve of a target using MAST data archive 
+
+    Parameters
+    ----------
+    name : `str`
+        Target name. Can be name of system, e.g. "Kepler-109", or particular planet, e.g. "Kepler-109 b"
+    mission : `str` 
+        Which mission to download data from, e.g. "Kepler", "K2", or "TESS"
+    exptime : `int` or "long", "short", "fast", optional 
+           Exposure time of desired data products. "long" selects 10 minute and 30 minute data products, 
+           "short" selects 1 and 2 minute data, and "fast" selects 20 second data. Can also pass an int of the 
+           exact exposure time in seconds. 
+           For example, exptime=1800 calls for 30 minute data products. 
+           By default, all cadence modes are returned (this may make it timeout). 
+
+    Returns
+    -------
+    flat_lc : `lightkurve LightCurve object`
+        Stitched and flattened light curve 
+    """     
+    # Search MAST data archive for target pixel files
+    search_results = lk.search_targetpixelfile(name, mission=mission, exptime=exptime)
+
+    # download the related TargetPixelFiles
+    tpfs = search_results.download_all()
+
+    # convert TPFs to light curves using Pixel Level Decorrelation
+    if method == "pipeline":
+        lcs = [tpf.to_lightcurve(aperture_mask="pipeline") for tpf in tpfs]
+        print("True")
+    else:
+        lcs = [tpf.to_lightcurve(method=method) for tpf in tpfs]
+    # collect light curves together
+    lcc = lk.LightCurveCollection(lightcurves=lcs)
+
+    # stitch the collection into a single light curve
+    # and flatten it (removes long-term trends using scipy’s Savitzky-Golay filter)
+    flat_lc = lcc.stitch().flatten()
+    return flat_lc
+
+
+def remove_outliers(lc, periods, t0s, durations, regular_sigma=5, transit_sigma=5.6, transit_sigma_upper=5):
+    """Removes outliers from light curve using sigma clipping (removing flux values that are greater or smaller 
+    than the median value by a number of standard deviations). Removes the most extreme outliers from the entire light curve, 
+    and removes "less extreme" outliers as long as there is no planet transiting. 
+
+    Parameters
+    ----------
+    lc : :class:`~lightkurve.Lightcurve` 
+        Stitched light curve of planetary system to remove outliers from 
+    
+    periods : `list`
+        A list of orbital periods (in days) of each planet in the system 
+    
+    t0s : `list`
+        A list of transit midpoints (in Julian dates) of each planet in the system 
+    
+    durations : `list`
+        A list of transit durations (in hours) of each planet in the system 
+    
+    regular_sigma : `float`
+        The number of standard deviations for both upper and lower clipping limit while no planet is transiting. Default is 5. 
+    
+    transit_sigma : `float`
+        The number of standard deviations for clipping limit during entire light curve (whether planet is transiting or not), to 
+        catch the most obvious outliers. Default is 5.6 (more leniant than regular_sigma). Overrided by transit_sigma_upper for 
+        upper clipping limit. 
+    
+    transit_sigma_upper : `float`
+        The number of standard deviations for upper clipping limit during entire light curve (whether planet is transiting or 
+        not), to catch the most obvious outliers. Default is 5 (more strict than transit_sigma because planets do not cause flux
+        values to go above median flux). 
+
+    Returns
+    -------
+    new_lc : :class:`~lightkurve.Lightcurve`
+        Light curve with extreme outliers removed during entire series and less extreme outliers removed outside of when 
+        a planet is transiting. 
+    """ 
+    # get two outlier masks, one more lenient than the other 
+    # returns points that have been removed 
+    _, outlier_mask = lc.remove_outliers(return_mask=True, sigma=regular_sigma)
+    _, extreme_outlier_mask = lc.remove_outliers(return_mask=True, sigma=transit_sigma,
+                                                 sigma_upper=transit_sigma_upper)
+
+    # count the number of planets in transit per timestep
+    total_in_transit = np.zeros(len(lc.time))
+    for p, t0, t_duration in zip(periods, t0s, durations):
+        phase = (lc.time - Time(t0, format="jd")).to(u.day).value % p
+        half_transit = 0.5 * (t_duration * u.hour).to(u.day).value
+        in_transit = (phase < half_transit) | (phase > p - half_transit)
+        total_in_transit += in_transit.astype(int)
+
+    # get a mask for whether there are *any* planets in transit
+    any_in_transit = total_in_transit > 0
+
+    # combine them into masks
+    no_extreme_outliers_ever = ~extreme_outlier_mask
+    no_regular_outliers_outside_transit = ~(outlier_mask & (~any_in_transit))
+
+    # return the fully masked lc
+    new_lc = lc[no_extreme_outliers_ever & no_regular_outliers_outside_transit]
+    return new_lc
---
+++ helpers/xo_archive.py
@@ -0,0 +1,169 @@
+from requests import request
+from collections import defaultdict
+import pandas as pd
+import numpy as np
+from astropy.time import Time 
+
+BASE_URL = "https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+"
+
+def get_exoplanet_parameters(search_name, which="default", custom_cond=None,
+                             columns=["pl_name", "pl_letter", "pl_orbper", "pl_orbincl", "pl_orbeccen",
+                                      "pl_trandep", "pl_tranmid", "pl_trandur", "pl_ratror",
+                                      "pl_imppar", "st_rad", "st_mass", "st_dens", "gaia_id", "pl_rade"]):
+    """Get parameters for exoplanets from the Exoplanet Archive using their TAP service
+
+    See here for more information: https://exoplanetarchive.ipac.caltech.edu/docs/TAP/usingTAP.html
+
+    Parameters
+    ----------
+    search_name : `str`
+        Substring against which to match the planet name, e.g. "Kepler-5 " <- the space is important so you
+        just get Kepler-5 and not Kepler-50 or Kepler-503 etc. Leave as `None` to ignore name conditions.
+    which : `str`, optional
+        Which table of parameters to draw from, one of ["default", "all", "composite"]. The default parameters
+        are from a single paper that is currently flagged as the main reference. "all" will give you many rows
+        for each planet, one row per publication per planet. "composite" combines the known parameters from
+        many papers into a single row - NOTE composite values may not be self-consistent. By default "default"
+    custom_cond : `str`, optional
+        Custom condition against which to match, by default None
+    columns : `list`, optional
+        Which columns to select from the tables. See here:
+        https://exoplanetarchive.ipac.caltech.edu/docs/API_PS_columns.html for a list of potential choices.
+        Note the columns are different for the "all" and "composite".
+        By default ["pl_name", "pl_letter", "pl_orbper", "pl_orbincl", "pl_orbeccen", "pl_trandep",
+                    "pl_tranmid", "pl_trandur", "pl_ratror", "pl_imppar", "st_dens", "pl_rade"]
+        Default units are:
+        "pl_orbper" (days), "pl_orbincl" (degrees), "pl_orbeccen" (degrees), "pl_transdep" (%), 
+        "pl_tranmid" (Julian date), "pl_trandur" (hours), "pl_ratro" (unitless), "pl_imppar" (unitless), 
+        "st_rad" (Solar radii), "st_mass" (Solar mass), "st_dens" (g/cm^3), "pl_rade" (Earth radii)
+
+
+    Returns
+    -------
+    parameters : `list`
+        A list of dictionaries, each corresponding to a row from the table
+
+    Examples
+    --------
+    A query for the composite parameters of the Kepler-444 system::
+        get_exoplanet_parameters("Kepler-444 ", which="composite")
+
+    A query for the default parameters of the Kepler-29 system::
+        get_exoplanet_parameters("Kepler-29 ")
+
+    A query for the default parameters of just Kepler-9b::
+        get_exoplanet_parameters("Kepler-9 ", custom_cond="pl_letter='b'")
+    """
+    # decide which table to pull from
+    table = "ps" if which in ["default", "all"] else "pscomppars"
+    from_table = f" from {table} where "
+
+    # specify conditions on the planet name and whether to get the default parameters
+    name_cond = f"lower(pl_name)+like+'%{search_name.lower()}%'" if search_name is not None else ""
+    default_cond = "default_flag=1" if which == "default" else ""
+    if search_name is not None and which == "default":
+        default_cond = " and " + default_cond
+
+    # add a custom condition if desired
+    if custom_cond is not None:
+        default_cond += f" and {custom_cond}"
+
+    # force the format to be JSON
+    fmt = "&format=JSON"
+
+    # combine into URL and perform the request
+    url = BASE_URL + ','.join(columns) + from_table + name_cond + default_cond + fmt
+
+    r = request(method="GET", url=url)
+
+    # print out an error if it failed, otherwise return the JSON
+    if r.status_code != 200:
+        print(f"Request failed with code {r.status_code}: Error message follows")
+        print("=======================================================")
+        print(r.text.rstrip())
+        print("=======================================================")
+        print(f"URL used: {url}")
+        
+    else:
+        parameters = r.json()
+
+        # convert gaia id strings to ints
+        gaia_ids = [int(p["gaia_id"].split(" ")[-1]) if p["gaia_id"] is not None else np.nan for p in parameters] 
+
+        # pass them to helper function and save Berger densities
+        densities = get_berger_density(gaia_ids=gaia_ids) 
+        for i in range(len(parameters)):
+            parameters[i]["berger_dens"] = densities[i]
+            #parameters[i]["pl_tranmid_bkjd"] = Time(parameters[i]["pl_tranmid"],format="jd").bkjd
+        return parameters
+
+
+def get_berger_density(gaia_ids):
+    """Get the stellar densities estimated by Berger+2023 associated with a collection of Gaia IDs
+
+    Parameters
+    ----------
+    gaia_ids : `list`
+        IDs for Gaia in DR3
+
+    Returns
+    -------
+    densities : `list`
+        Densities in g/cm^3
+        
+    masses: `list`
+        Masses in solar units 
+    """
+    # yes, this is such a hack please don't judge me
+    base = __file__.replace("/xo_archive.py", "/")
+    stellar_df = pd.read_csv(base + "GKTHCatalog_Table4.csv")
+    gaia_df = pd.read_csv(base + "GKTHCatalog_Table2.csv")
+    
+    densities = np.repeat(-1, len(gaia_ids)).astype(float) 
+    masses = np.repeat(-1, len(gaia_ids)).astype(float) 
+        
+    for i in range(len(gaia_ids)): 
+        if np.isnan(gaia_ids[i]) == False: 
+            in_gaia_table = gaia_df["dr3_source_id"] == gaia_ids[i]
+            star_id = gaia_df[in_gaia_table]["id_starname"].values.tolist()
+                
+            if star_id != []:
+                in_stellar_table = stellar_df["id_starname"] == star_id[0]
+                density = stellar_df[in_stellar_table]["iso_rho"].values.tolist()
+                if len(density)>1:
+                    print('error! gaia id found to have more than one berger density') 
+                densities[i] = density[0]  
+                
+                mass = stellar_df[in_stellar_table]["iso_mass"].values.tolist()
+                if len(mass)>1:
+                    print('error! gaia id found to have more than one berger mass') 
+                masses[i] = mass[0] 
+                
+            #else:
+            #    print('not in table!') 
+               
+            if len(star_id)>1:
+                print('error! gaia id found to have more than one stellar id ') 
+        else:
+            print('no gaia id found!') 
+    return densities
+
+def transpose_parameters(parameters):
+    """Transform a list of dictionaries of parameters into a dictionary of lists
+
+    Parameters
+    ----------
+    parameters : `list`
+        List of dictionaries of parameters
+
+    Returns
+    -------
+    transposed : `dict`
+        Dictionary of columns, each of which are a list of values (one for each planet)
+    """
+    transposed = defaultdict(list)
+    keys = parameters[0].keys()
+    for i in range(len(parameters)):
+        for key in keys:
+            transposed[key].append(parameters[i][key])
+    return dict(transposed)
