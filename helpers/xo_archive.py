5f04be63be6c3cc5d86e6a3a14b81fef4de74721
Gabrielle Suissa <ge2205@columbia.edu>
2024-05-15 19:03:55 -0700

    made code style-guide compliant

--- helpers/xo_archive.py
+++ helpers/xo_archive.py
@@ -2,14 +2,33 @@ from requests import request
 from collections import defaultdict
 import pandas as pd
 import numpy as np
-from astropy.time import Time 
+from astropy.time import Time
 
 BASE_URL = "https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+"
 
-def get_exoplanet_parameters(search_name, which="default", custom_cond=None,
-                             columns=["pl_name", "pl_letter", "pl_orbper", "pl_orbincl", "pl_orbeccen",
-                                      "pl_trandep", "pl_tranmid", "pl_trandur", "pl_ratror",
-                                      "pl_imppar", "st_rad", "st_mass", "st_dens", "gaia_id", "pl_rade"]):
+
+def get_exoplanet_parameters(
+    search_name,
+    which="default",
+    custom_cond=None,
+    columns=[
+        "pl_name",
+        "pl_letter",
+        "pl_orbper",
+        "pl_orbincl",
+        "pl_orbeccen",
+        "pl_trandep",
+        "pl_tranmid",
+        "pl_trandur",
+        "pl_ratror",
+        "pl_imppar",
+        "st_rad",
+        "st_mass",
+        "st_dens",
+        "gaia_id",
+        "pl_rade",
+    ],
+):
     """Get parameters for exoplanets from the Exoplanet Archive using their TAP service
 
     See here for more information: https://exoplanetarchive.ipac.caltech.edu/docs/TAP/usingTAP.html
@@ -33,8 +52,8 @@ def get_exoplanet_parameters(search_name, which="default", custom_cond=None,
         By default ["pl_name", "pl_letter", "pl_orbper", "pl_orbincl", "pl_orbeccen", "pl_trandep",
                     "pl_tranmid", "pl_trandur", "pl_ratror", "pl_imppar", "st_dens", "pl_rade"]
         Default units are:
-        "pl_orbper" (days), "pl_orbincl" (degrees), "pl_orbeccen" (degrees), "pl_transdep" (%), 
-        "pl_tranmid" (Julian date), "pl_trandur" (hours), "pl_ratro" (unitless), "pl_imppar" (unitless), 
+        "pl_orbper" (days), "pl_orbincl" (degrees), "pl_orbeccen" (degrees), "pl_transdep" (%),
+        "pl_tranmid" (Julian date), "pl_trandur" (hours), "pl_ratro" (unitless), "pl_imppar" (unitless),
         "st_rad" (Solar radii), "st_mass" (Solar mass), "st_dens" (g/cm^3), "pl_rade" (Earth radii)
 
 
@@ -59,7 +78,11 @@ def get_exoplanet_parameters(search_name, which="default", custom_cond=None,
     from_table = f" from {table} where "
 
     # specify conditions on the planet name and whether to get the default parameters
-    name_cond = f"lower(pl_name)+like+'%{search_name.lower()}%'" if search_name is not None else ""
+    name_cond = (
+        f"lower(pl_name)+like+'%{search_name.lower()}%'"
+        if search_name is not None
+        else ""
+    )
     default_cond = "default_flag=1" if which == "default" else ""
     if search_name is not None and which == "default":
         default_cond = " and " + default_cond
@@ -83,18 +106,21 @@ def get_exoplanet_parameters(search_name, which="default", custom_cond=None,
         print(r.text.rstrip())
         print("=======================================================")
         print(f"URL used: {url}")
-        
+
     else:
         parameters = r.json()
 
         # convert gaia id strings to ints
-        gaia_ids = [int(p["gaia_id"].split(" ")[-1]) if p["gaia_id"] is not None else np.nan for p in parameters] 
+        gaia_ids = [
+            int(p["gaia_id"].split(" ")[-1]) if p["gaia_id"] is not None else np.nan
+            for p in parameters
+        ]
 
         # pass them to helper function and save Berger densities
-        densities = get_berger_density(gaia_ids=gaia_ids) 
+        densities = get_berger_density(gaia_ids=gaia_ids)
         for i in range(len(parameters)):
             parameters[i]["berger_dens"] = densities[i]
-            #parameters[i]["pl_tranmid_bkjd"] = Time(parameters[i]["pl_tranmid"],format="jd").bkjd
+            # parameters[i]["pl_tranmid_bkjd"] = Time(parameters[i]["pl_tranmid"],format="jd").bkjd
         return parameters
 
 
@@ -110,43 +136,44 @@ def get_berger_density(gaia_ids):
     -------
     densities : `list`
         Densities in g/cm^3
-        
+
     masses: `list`
-        Masses in solar units 
+        Masses in solar units
     """
     # yes, this is such a hack please don't judge me
     base = __file__.replace("/xo_archive.py", "/")
     stellar_df = pd.read_csv(base + "GKTHCatalog_Table4.csv")
     gaia_df = pd.read_csv(base + "GKTHCatalog_Table2.csv")
-    
-    densities = np.repeat(-1, len(gaia_ids)).astype(float) 
-    masses = np.repeat(-1, len(gaia_ids)).astype(float) 
-        
-    for i in range(len(gaia_ids)): 
-        if np.isnan(gaia_ids[i]) == False: 
+
+    densities = np.repeat(-1, len(gaia_ids)).astype(float)
+    masses = np.repeat(-1, len(gaia_ids)).astype(float)
+
+    for i in range(len(gaia_ids)):
+        if np.isnan(gaia_ids[i]) == False:
             in_gaia_table = gaia_df["dr3_source_id"] == gaia_ids[i]
             star_id = gaia_df[in_gaia_table]["id_starname"].values.tolist()
-                
+
             if star_id != []:
                 in_stellar_table = stellar_df["id_starname"] == star_id[0]
                 density = stellar_df[in_stellar_table]["iso_rho"].values.tolist()
-                if len(density)>1:
-                    print('error! gaia id found to have more than one berger density') 
-                densities[i] = density[0]  
-                
+                if len(density) > 1:
+                    print('error! gaia id found to have more than one berger density')
+                densities[i] = density[0]
+
                 mass = stellar_df[in_stellar_table]["iso_mass"].values.tolist()
-                if len(mass)>1:
-                    print('error! gaia id found to have more than one berger mass') 
-                masses[i] = mass[0] 
-                
-            #else:
-            #    print('not in table!') 
-               
-            if len(star_id)>1:
-                print('error! gaia id found to have more than one stellar id ') 
+                if len(mass) > 1:
+                    print('error! gaia id found to have more than one berger mass')
+                masses[i] = mass[0]
+
+            # else:
+            #    print('not in table!')
+
+            if len(star_id) > 1:
+                print('error! gaia id found to have more than one stellar id ')
         else:
-            print('no gaia id found!') 
+            print('no gaia id found!')
     return densities
+
 
 def transpose_parameters(parameters):
     """Transform a list of dictionaries of parameters into a dictionary of lists
